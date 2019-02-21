2ec94259ca004f8cd10121abdfa73e3bff2e3e43
pedro <pedro@debiancli>
2019-02-21 16:13:27 +0000

    Commit, mais páginas web ->Insere bagagens ->Info Bagagens ->InfoAviao

--- TP-warWebSite/src/java/intermediario/BagabemBean.java
+++ TP-warWebSite/src/java/intermediario/BagabemBean.java
@@ -14,6 +14,7 @@ import javax.ejb.LocalBean;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import tpdtos.BagagemDTO;
+import tpdtos.ClienteDTO;
 import tpdtos.ViagemDTO;
 
 /**
@@ -23,30 +24,37 @@ import tpdtos.ViagemDTO;
 @Named(value = "bagagemBean")
 @ManagedBean
 @SessionScoped
-public class BagabemBean implements Serializable{
+public class BagabemBean implements Serializable {
+
     private int id_bagagem;
     private int peso_bagagem;
-    private ViagemDTO viagem;  
-     
-     @EJB
-     intermedioLogicaLocal acessoLogica;
-     
+    private ViagemDTO viagem;
+    private int id_cliente;
+
+    @EJB
+    intermedioLogicaLocal acessoLogica;
+
 //     @EJB
 //     ClienteBean cliente;
-     
-    public BagabemBean(){
-        
+    public BagabemBean() {
+
     }
-    
-    
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
     public int getId_bagagem() {
         return id_bagagem;
     }
 
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    
     public void setId_bagagem(int id_bagagem) {
         this.id_bagagem = id_bagagem;
     }
@@ -66,19 +74,61 @@ public class BagabemBean implements Serializable{
     public void setViagem(ViagemDTO viagem) {
         this.viagem = viagem;
     }
-    
-    public List<BagagemDTO> selecionaAllBagagens(){
+
+    public List<BagagemDTO> selecionaAllBagagens() {
         /*DEPOIS DE FAZER LOGIN, SEI QUAL É O UTILIZADOR, E ENCONTRA-SE DEFINIDO NA CLASSE SESSIONCONTEXT*/
-        String cli= (String)SessionContext.getInstance().getAttribute("cli");
+        String cli = (String) SessionContext.getInstance().getAttribute("cli");
         //this.cliente.setEmail_cliente(cli);
-        
-        List<BagagemDTO> bagagens= this.acessoLogica.getSingletonLogica().SelecionaBagagens(cli);
-        if(bagagens==null){
+
+        List<BagagemDTO> bagagens = this.acessoLogica.getSingletonLogica().SelecionaBagagens(cli);
+        if (bagagens == null) {
             return null;
         }
-        
+
         return bagagens;
     }
 
-   
+    public String insereBagagem() {
+        try {
+            int id_cliente = (int) SessionContext.getInstance().getAttribute("id");
+            boolean estado_criacao_bagagem = this.acessoLogica.getSingletonLogica().insertBagagem(peso_bagagem, viagem.getId(), id_cliente);
+            if (estado_criacao_bagagem == true) {
+                return "/index.xhtml?faces-redirect=true?";
+            }
+            return "/erro.xhtml?faces-redirect=true?";
+        } catch (Exception e) {
+            System.out.println("" + e.getMessage());
+            return null;
+        }
+    }
+    
+    public List<ClienteDTO> obtemListaClientes(){
+        try{
+            List<ClienteDTO> lista_clientes=this.acessoLogica.getSingletonLogica().seleccionaAllClientes();
+            if(lista_clientes==null){
+                return null;
+            }
+            return lista_clientes;
+        }catch(Exception e){
+            System.out.println(""+e.getMessage());
+            return null;
+        }
+    }
+    
+    public List<ViagemDTO> obtemListaViagens(){
+        try{
+            List<ViagemDTO> lista_viagens= this.acessoLogica.getSingletonLogica().seleccionaAllViagens();
+            if(lista_viagens==null){
+                return null;
+            }
+            return lista_viagens;
+        } catch(Exception e){
+            System.out.println(""+e.getMessage());
+            return null;
+        }
+    }
+    
+    
+
+
 }
